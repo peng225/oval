a5ab2e78b9fc8f8b13076c4e80569766a2dbe410
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-05-29 15:34:32 +0900

    implement Run() function.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- validator/run.go
+++ validator/run.go
@@ -1,5 +1,10 @@
 package validator
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Validator struct {
 	NumObj     int64
 	NumWorker  int
@@ -10,5 +15,49 @@ type Validator struct {
 }
 
 func (v *Validator) Run() {
+	for round := 0; round < v.NumRound; round++ {
+		fmt.Printf("round: %v\n", round)
+		// Create phase
+		wg := &sync.WaitGroup{}
+		for i := 0; i < v.NumWorker; i++ {
+			wg.Add(1)
+			go func() {
+				v.create()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
 
+		// Update phase
+		for i := 0; i < v.NumWorker; i++ {
+			wg.Add(1)
+			go func() {
+				v.update()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		// Delete phase
+		for i := 0; i < v.NumWorker; i++ {
+			wg.Add(1)
+			go func() {
+				v.delete()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func (v *Validator) create() {
+	// TODO: implement
+}
+
+func (v *Validator) update() {
+	// TODO: implement
+}
+
+func (v *Validator) delete() {
+	// TODO: implement
 }
