2b7bc4af3b0d6f0a7cf3cf484ff24319134f6eeb
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-05-29 14:54:25 +0900

    add the initial main function etc.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/peng225/oval/parser"
+)
+
+func main() {
+	var (
+		numObj      int64
+		numWorker   int
+		sizePattern string
+		numRound    int
+		bucketName  string
+	)
+	flag.Int64Var(&numObj, "num_obj", 10, "The number of objects.")
+	flag.IntVar(&numWorker, "num_worker", 1, "The number of workers.")
+	flag.StringVar(&sizePattern, "size", "4k", "The size of object. Should be in the form like \"4k-2m\" or \"8k\".")
+	flag.IntVar(&numRound, "round", 1, "The number of test rounds.")
+	flag.StringVar(&bucketName, "bucket", "", "The name of the bucket.")
+	flag.Parse()
+
+	minSize, maxSize, err := parser.SizeParse(sizePattern)
+}
---
+++ parser/size_parser.go
@@ -0,0 +1,37 @@
+package parser
+
+import (
+	"strconv"
+	"strings"
+)
+
+func SizeParse(s string) (int, int, error) {
+	s = strings.ToLower(s)
+	var sizesStr []string
+	if strings.Contains(s, "-") {
+		sizesStr = strings.Split(s, "-")
+		if len(sizesStr) != 2 {
+			panic("len(sizesStr) != 2")
+		}
+	} else {
+		sizesStr = []string{s}
+	}
+
+	sizes := make([]int, 2)
+	for i, sizeStr := range sizesStr {
+		sizeStrByte, err := parseSizeUnit(sizeStr)
+		if err != nil {
+			return 0, 0, err
+		}
+		sizes[i], err = strconv.Atoi(sizeStrByte)
+		if err != nil {
+			return 0, 0, err
+		}
+	}
+	return sizes[0], sizes[1], nil
+}
+
+func parseSizeUnit(s string) (string, error) {
+	// TODO: implement
+	return s, nil
+}
